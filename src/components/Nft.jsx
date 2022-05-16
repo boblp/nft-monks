a36059564e47c01703222bc7e3375a2523c7ead0
BobXplosion <bob0011@hotmail.com>
2022-05-15 21:29:34 -0500

    cleanup

--- src/components/Nft.jsx
+++ src/components/Nft.jsx
@@ -1,12 +1,32 @@
+import mergeImages from 'merge-images';
 import '../scss/nftImages.scss';
 
 export default function Nft({ nftObject }) {
+  const {
+    backgrounds,
+    bodies,
+    faces,
+    hats,
+  } = nftObject;
+
+  const download = () => {
+    mergeImages([
+      `./images/test_images/backgrounds/${backgrounds}.png`,
+      `./images/test_images/bodies/${bodies}.png`,
+      `./images/test_images/faces/${faces}.png`,
+      `./images/test_images/hats/${hats}.png`
+    ]).then((b64) => {
+      console.log(b64)
+    });
+  }
+
   return (
-    <div class="nft-box">
-      <img class="nft-img" src={require(`../images/test_images/backgrounds/${nftObject.backgrounds}.png`)} alt="not found" />
-      <img class="nft-img" src={require(`../images/test_images/bodies/${nftObject.bodies}.png`)} alt="not found" />
-      <img class="nft-img" src={require(`../images/test_images/faces/${nftObject.faces}.png`)} alt="not found" />
-      <img class="nft-img" src={require(`../images/test_images/hats/${nftObject.hats}.png`)} alt="not found" />
+    <div className="nft-box">
+      <button onClick={() => download()}>Download</button>
+      <img className="nft-img" src={require(`../images/test_images/backgrounds/${backgrounds}.png`)} alt="not found" />
+      <img className="nft-img" src={require(`../images/test_images/bodies/${bodies}.png`)} alt="not found" />
+      <img className="nft-img" src={require(`../images/test_images/faces/${faces}.png`)} alt="not found" />
+      <img className="nft-img" src={require(`../images/test_images/hats/${hats}.png`)} alt="not found" />
     </div>
   );
 };
