dff8d3c9d5971365c33f77a59ae600bcc3eeece6
yannicksprenger <yannick.sprenger@gmail.com>
2022-05-17 15:05:17 -0500

    naming conventions

--- src/pages/NftSpecifics.jsx
+++ src/pages/NftSpecifics.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import {trades} from '../trades';
 import Form from '../components/attributesForm';
 
-export default function nftSpecifics () {
+export default function NftSpecifics () {
     return (
         <Form trades={trades} />
     )
